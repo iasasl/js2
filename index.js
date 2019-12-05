e969d086e1f9a15be01477a24a4a1565b12cc977
iasasl <v1lat94@mail.ru>
2019-12-05 14:31:16 +0300

    Добавлен каркас

---
+++ index.js
@@ -0,0 +1,17 @@
+const goods = [
+    { title: 'iPhone', price: 150 },
+    { title: 'Samsung', price: 250 },
+    { title: 'Xiaomi', price: 350 },
+    { title: 'Huawei', price: 450 }
+];
+
+const renderGoodsItem = (title, price) => {
+    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+};
+
+const renderGoodsList = (list) => {
+    const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+    document.querySelector('.goods-list').innerHTML = goodsList;
+};
+
+renderGoodsList(goods);
