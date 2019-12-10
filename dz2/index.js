b57474697b4cff856f2a1c96e01b23c3beec91d0
iasasl <v1lat94@mail.ru>
2019-12-10 13:06:06 +0300

    Исправлены ошибки из предыдущего домашнего задания и решены новые

--- dz2/index.js
+++ dz2/index.js
@@ -5,25 +5,10 @@ const goods = [
     { title: 'Huawei', price: 450 }
 ];
 
-const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}&#8381;</p></div>`;
-};
-
-const renderGoodsList = (list) => {
-    const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
-};
-
-renderGoodsList(goods);
-
 class GoodsList {
     constructor(id, amount) {
         this.id = id,
         this.amount = amount
-    }
-
-    countSum() {
-        
     }
 };
 
@@ -34,3 +19,14 @@ class GoodsListItem {
         this.price = price
     }
 }
+
+const renderGoodsItem = (title, price) => {
+    return `<div class="goods-item"><h3>${title}</h3><p>${price}&#8381;</p></div>`;
+};
+
+const renderGoodsList = (list) => {
+    const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+    document.querySelector('.goods-list').innerHTML = goodsList.join('');
+};
+
+renderGoodsList(goods);
